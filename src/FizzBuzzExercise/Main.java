98f6faf3f29f450a297c56f2b1e663c246f71aea
Aline Ebone <iniebone@gmail.com>
2018-07-07 19:40:03 -0300

    FizzBuzz exercise.

--- src/FizzBuzzExercise/Main.java
+++ src/FizzBuzzExercise/Main.java
@@ -1,4 +1,9 @@
 package FizzBuzzExercise;
 
 public class Main {
+
+    public static void main(String[] args) {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+    }
+
 }
---
+++ src/PrimeFactorsExercise/PrimeFactors.java
@@ -0,0 +1,5 @@
+package PrimeFactorsExercise;
+
+public class PrimeFactors {
+
+}
