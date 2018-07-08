994e20fb16ea35afe6071a3098a23dfa6132f2bd
Aline Ebone <iniebone@gmail.com>
2018-07-07 22:08:58 -0300

    PrimeFactors

--- src/PrimeFactorsExercise/Main.java
+++ src/PrimeFactorsExercise/Main.java
@@ -1,24 +1,12 @@
 package PrimeFactorsExercise;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        int number = 1;
-        Set primefactors = new HashSet<>();
-        long copyOfInput = number;
+        PrimeFactors primeFactors = new PrimeFactors();
 
-        for (int i = 2; i <= copyOfInput; i++) {
-            if (copyOfInput % i == 0) {
-                primefactors.add(i); // prime factor
-                copyOfInput /= i;
-                i--;
-            }
-        }
-        System.out.println(primefactors);
+        System.out.println(primeFactors.PrimeFactors(30));
 
     }
 }
--- src/PrimeFactorsExercise/PrimeFactors.java
+++ src/PrimeFactorsExercise/PrimeFactors.java
@@ -1,5 +1,24 @@
 package PrimeFactorsExercise;
 
+import java.util.ArrayList;
+
 public class PrimeFactors {
 
+    public ArrayList<Integer> PrimeFactors(int n) {
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        int copy = n;
+
+        for (int i = 2; i <= copy; i++) {
+            if (copy % i == 0) {
+                numbers.add(i);
+                copy = copy / i;
+                i--;
+            }
+        }
+
+        return numbers;
+
+    }
 }
